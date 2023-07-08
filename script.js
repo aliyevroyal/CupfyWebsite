2c6b3a7de93b0bba5c9cfcac6570cec7c34ae338
aliyevroyal <royal.alyv@gmail.com>
2023-07-08 11:35:26 +0400

    Initial commit

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-document.getElementById("monthly").onclick = subscribeMonthly;
+document.getElementById("15Cups").onclick = subscribeMonthly;
 function subscribeMonthly() {
-  document.getElementById("monthly").style.backgroundColor = "#14cc60";
-  document.getElementById("monthly").style.color = "#f5f5f5";
-  document.getElementById("yearly").style.backgroundColor = "#f5f5f5";
-  document.getElementById("yearly").style.color = "#0a2e36";
+  document.getElementById("15Cups").style.backgroundColor = "#14cc60";
+  document.getElementById("15Cups").style.color = "#f5f5f5";
+  document.getElementById("30Cups").style.backgroundColor = "#f5f5f5";
+  document.getElementById("30Cups").style.color = "#0a2e36";
 
-  $("#YearlySubscription").css({ display: "none" });
-  $("#MonthlySubscription").css({ display: "flex" });
+  $("#30CupsSubscription").css({ display: "none" });
+  $("#15CupsSubscription").css({ display: "flex" });
 }
 
-document.getElementById("yearly").onclick = subscribeYearly;
+document.getElementById("30Cups").onclick = subscribeYearly;
 function subscribeYearly() {
-  document.getElementById("yearly").style.backgroundColor = "#14cc60";
-  document.getElementById("yearly").style.color = "#f5f5f5";
-  document.getElementById("monthly").style.backgroundColor = "#f5f5f5";
-  document.getElementById("monthly").style.color = "#0a2e36";
+  document.getElementById("30Cups").style.backgroundColor = "#14cc60";
+  document.getElementById("30Cups").style.color = "#f5f5f5";
+  document.getElementById("15Cups").style.backgroundColor = "#f5f5f5";
+  document.getElementById("15Cups").style.color = "#0a2e36";
 
-  $("#MonthlySubscription").css({ display: "none" });
-  $("#YearlySubscription").css({ display: "flex" });
+  $("#15CupsSubscription").css({ display: "none" });
+  $("#30CupsSubscription").css({ display: "flex" });
 }
